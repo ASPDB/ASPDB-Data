a5f2f38ae9c66bd92249e76152b8ff7927bc95a9
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-14 14:30:10 +0530

    Initial commit
    build.sh, shell script to --/build/-- whole "ASPDB®" in one go.
    Usage: $ sh build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# ==========================++== ASPDB® ==++==========================
+#      A manually curated dedicated acid shock proteins database.
+#
+#                                 CCPE
+#             Centre for Computational Protein Engineering
+#
+#                            Apache License
+#                    http://www.apache.org/licenses/
+#
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.
+# Copyright © 2014 - 2016, Sequømics Corporation, All rights reserved.
+# ====================================================================
