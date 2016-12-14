2fdbca3a01f33e1bf9b0eff575e1085196359354
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-14 21:08:02 +0530

    Initial commit
    SEED™: [Skövde] Build System — http://seed.sequomics.com/.
    A Sequømics Product — http://sequomics.com/.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,31 @@
 #!/usr/bin/env node
+/*!
+ * -----------
+ * ASPDB®-Data
+ * -----------
+ * Data of ASPDB — A manually curated dedicated acid shock proteins database.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 10-Dec-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : SEED™ — Skövde
+ *              └────── A Sequømics Product — http://seed.sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "ASPDB®-Data".
+ * ___________________________________________________________________________
+ */
