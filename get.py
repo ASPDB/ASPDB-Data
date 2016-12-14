39d60976b14e25f01e556122ba3bffe15cf080eb
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-14 14:51:11 +0530

    Initial commit
    get.py — To get the list of searched proteins from the Protein Databank.
    Help: https://www.python.org/ | {Python ≥ 2.7.12}.

---
+++ get.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ————————————————————————————————————————————————————————————————————————————
+# Copyright © 2014 - 2016, Sequømics Research, All rights reserved.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ————————————————————————————————————————————————————————————————————————————
+# Licensed under the Apache License (the "License, Version 2.0");
+# you may not use this file except in compliance with the License.
+# ————————————————————————————————————————————————————————————————————————————
