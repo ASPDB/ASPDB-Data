2291ddce52ff131d108e0e9894cb84f2e061c839
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-15 14:41:21 +0530

    Initial commit
    A smart stripBOM  — {function} Defined.
    Copyright © 2008 - 2016, Prabhat Kumar, All rights reserved. » http://prabhatkumar.org/.

--- version.js
+++ version.js
@@ -32,3 +32,16 @@
 // To load required Node module.
 // -----------------------------
 var fs    = require('fs');
+
+///--------------------------
+// A smart stripBOM function.
+///----------- 01 -----------
+function stripBOM(content) {
+  // Remove byte order marker. This catches EF BB BF (the UTF-8 BOM)
+  // because the buffer-to-string conversion in `fs.readFileSync()`
+  // translates it to FEFF, the UTF-16 BOM.
+  if (content.charCodeAt(0) === 0xFEFF) {
+    content = content.slice(1);
+  }
+  return content;
+}
